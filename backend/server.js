28f15f81299718658b765fa8c96ff8f0d3403991
Palak Singh <singhpalakk659@gmail.com>
2026-09-04 00:31:45 +0530

    connected frontend and server

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,19 @@
 import express from 'express';
+import cors from 'cors'
+import {generate} from './chatbot.js';
 const app = express();
 const port = 3001;
 
-import {generate} from './chatbot.js';
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to CatGPT')
 });
 
-
-
 app.post('/chat', async (req, res) =>{
     const {message}  = req.body;
-
-    console.log('Message', message);
 
     const result = await generate(message);
 
--- frontend/script.js
+++ frontend/script.js
@@ -5,20 +5,45 @@ const askBtn = document.querySelector('#ask');
 input.addEventListener('keyup', handleEnter);
 askBtn?.addEventListener('click', handleAsk);
 
-function generate(text){
+async function generate(text){
     const msg = document.createElement('div');
-    msg.className = 'my-6 bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit';
+    msg.className = 'my-6  bg-neutral-800 p-3 rounded-xl ml-auto max-w-fit'
     msg.textContent = text;
     chatContainer?.appendChild(msg);
     input.value = '';
+
+    const assistantMessage = await callServer(text);
+
+    const assistantMsgElem = document.createElement('div');
+    assistantMsgElem.className = 'max-w-fit';
+    assistantMsgElem.textContent = assistantMessage;
+    chatContainer?.appendChild(assistantMsgElem);
 }
 
-function handleAsk(e){
-     const text = input?.value.trim();
+async function callServer(inputText){
+    const response = await fetch('http://localhost:3001/chat', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+        },
+        body: JSON.stringify({message: inputText}),
+    });
+
+    if(!response.ok){
+        throw new Error("Error generating the response.");
+    }
+
+    const result =  await response.json();
+    return result.message;
+
+}
+
+ async function handleAsk(e){
+    const text = input?.value.trim();
     if(!text){
         return;
     }
-    generate(text);
+    await generate(text);
 }
 
 
